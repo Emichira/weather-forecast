3a835bee6dd355aad183542753951e60b0dee767
Emmanuel Michira <emmanuelmichira@gmail.com>
2020-09-11 23:52:36 +0300

    fix bugs implement add to home screen

--- public/main.js
+++ public/main.js
@@ -107,10 +107,11 @@ if ("serviceWorker" in navigator) {
 
 // Listen for beforeinstallprompt
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     // Update UI notify the user they can add to home screen
@@ -128,17 +129,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
     addBtn.addEventListener('click', (e) => {
         // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
+        // addBtn.style.display = 'none';
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
         deferredPrompt.userChoice.then((choiceResult) => {
             if (choiceResult.outcome === 'accepted') {
                 console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
             }
             deferredPrompt = null;
         });
     });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    app.logEvent('a2hs', 'installed');
 });
